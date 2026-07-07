a51a48b966a0bd9234bc06f71aedea8608d324e2
ranielhy <ranielhy@gmail.com>
2026-07-07 17:12:58 -0300

    inicio da extension

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+function App() {
+    return (
+        <Container sx={{ py: 3, width: 380 }}>
+            <Typography variant="h5" align="center">
+                Poster Print
+            </Typography>
+        </Container>
+    );
+}
+
+export default App;
