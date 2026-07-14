9e6cfc2b75c18656e99a36006c35e2e774ead7cb
ranielhy <ranielhy@gmail.com>
2026-07-14 11:05:49 -0300

    Configura deploy no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,28 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//     plugins: [react()],
+//     build: {
+//         outDir: "dist",
+//         rollupOptions: {
+//             input: {
+//                 main: "index.html",
+//                 background: "background.js"
+//             },
+//             output: {
+//                 entryFileNames: "[name].js",
+//                 chunkFileNames: "assets/[name]-[hash].js",
+//                 assetFileNames: "assets/[name]-[hash][extname]"
+//             }
+//         }
+//     }
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: "dist",
-        rollupOptions: {
-            input: {
-                main: "index.html",
-                background: "background.js"
-            },
-            output: {
-                entryFileNames: "[name].js",
-                chunkFileNames: "assets/[name]-[hash].js",
-                assetFileNames: "assets/[name]-[hash][extname]"
-            }
-        }
-    }
+	plugins: [react()],
+	base: "/poster-print-extension/",
 });
